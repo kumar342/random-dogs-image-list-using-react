813347dfc5b054b91d891dd01743cbfe86ed50b5
Hemanth Kumar <56812697+kumar342@users.noreply.github.com>
2020-06-06 21:49:10 +0530

    dashboard added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,38 +1,59 @@
 import React, { Component } from "react";
-import { FormGroup, Label, Input } from "reactstrap";
 import axios from "axios";
 
-export default class Dashboard extends Component {
+export default class dashboard extends Component {
   state = {
-    data: "",
+    data: [],
+    Value: "",
   };
-  componentDidMount = () => {
-    axios
-      .get("https://dog.ceo/api/breeds/list/all")
-      .then((res) => {
-        console.log(res.data);
-        let x = Object.keys(res.data);
-        console.log(x);
 
-        this.setState({ data: x });
+  componentDidMount() {
+    this.getDogsapi();
+  }
+
+  getDogsapi = () => {
+    axios
+      .get(`https://dog.ceo/api/breeds/list/all`)
+      .then((res) => {
+        const a = Object.entries(res.data.message)
+          .map((k) => (k[1].length ? k[1].map((i) => i + " " + k[0]) : k[0]))
+          .flat();
+
+        this.setState({ data: a });
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
   render() {
     return (
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input
-          type="select"
-          name="selectMulti"
-          id="exampleSelectMulti"
-          multiple
-        >
-          <option>{this.state.data}</option>
-        </Input>
-      </FormGroup>
+      <div>
+        <div>
+          <h2> Hey! </h2>
+          <h3>Here the dogs list and you can select which dog you want.</h3>
+        </div>
+
+        <form>
+          <ul className="list-unstyled">
+            {this.state.data.map((i) => {
+              return (
+                <li>
+                  <hr />
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    value={i}
+                    id={i}
+                  >
+                    {i}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </form>
+      </div>
     );
   }
 }
